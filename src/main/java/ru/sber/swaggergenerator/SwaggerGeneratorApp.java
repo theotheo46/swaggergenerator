2017921f32cc9731e71752a32cf1a31325a0f1aa
theotheo46 <theotheo46@gmail.com>
2022-11-24 11:49:44 +0300

    First commit

--- src/main/java/ru/sber/swaggergenerator/SwaggerGeneratorApp.java
+++ src/main/java/ru/sber/swaggergenerator/SwaggerGeneratorApp.java
@@ -4,7 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SwaggerGeneratorApp {
-
     public static void main(String[] args) {
         SpringApplication.run(SwaggerGeneratorApp.class, args);
     }
